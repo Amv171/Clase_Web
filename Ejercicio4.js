d78124d2a59b40f0f6f5ebb863f3cde52c862e83
Antonio Moreno <moreno.villas.17@gmail.com>
2023-09-26 11:44:13 +0100

    Format review

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -1,16 +1,16 @@
 let cont = 0;
 const fotos = ["img/1.jpg", "img/2.jpg", "img/3.jpg"];
-const prev = ()=>{
-    cont--;
-    if(cont<0){
-        cont = fotos.length-1;
-    }
-    document.getElementById("image").src = fotos[cont];
-}
-const next = ()=>{
-    cont++;
-    if(cont>=fotos.length){
-        cont = 0;
-    }
-    document.getElementById("image").src = fotos[cont];
-}
+const prev = () => {
+  cont--;
+  if (cont < 0) {
+    cont = fotos.length - 1;
+  }
+  document.getElementById("image").src = fotos[cont];
+};
+const next = () => {
+  cont++;
+  if (cont >= fotos.length) {
+    cont = 0;
+  }
+  document.getElementById("image").src = fotos[cont];
+};
