f52e25253670cd2a4ef5cff2c9928348da967d3c
Antonio Moreno <moreno.villas.17@gmail.com>
2023-10-10 17:10:30 +0100

    Strting to make it work

---
+++ Practica1/js/js.js
@@ -0,0 +1,6 @@
+const play = (jugadores)=>{
+    alert("Jugadores: "+jugadores);
+}
+document.getElementById("0jugador").onclick = ()=>{play(0);}
+document.getElementById("1jugador").onclick = ()=>{play(1);}
+document.getElementById("2jugador").onclick = ()=>{play(2);}
